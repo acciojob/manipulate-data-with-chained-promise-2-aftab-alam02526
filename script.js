775ce2a9a2cddff96cf96972e431cd49885af71e
aftab-alam02526 <aftab0447@gmail.com>
2025-10-04 12:16:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getNumbers() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000); 
+  });
+}
+
+const outputDiv = document.getElementById("output");
+
+
+getNumbers()
+  .then(numbers => {
+    const evens = numbers.filter(num => num % 2 === 0);
+
+    return delay(1000).then(() => {
+      outputDiv.textContent = evens; // update UI with [2,4]
+      return evens;
+    });
+  })
+  .then(evens => {
+    const doubled = evens.map(num => num * 2);
+
+    return delay(2000).then(() => {
+      outputDiv.textContent = doubled;
+      return doubled;
+    });
+  })
+  .catch(err => console.error(err));
